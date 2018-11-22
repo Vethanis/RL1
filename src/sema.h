d171f9db59c983345c1ce55987b1601ce9970eb8
Lauren Hughes <lh0xfb@gmail.com>
2018-11-21 23:53:40 -0800

    simplify sema.h

--- src/sema.h
+++ src/sema.h
@@ -3,39 +3,6 @@
 #include <mutex>
 #include <condition_variable>
 
-struct Semaphore
-{
-    std::mutex              m_mutex;
-    std::condition_variable m_cvar;
-    uint64_t                m_count = 0;
-
-    void Signal() 
-    {
-        std::lock_guard<decltype(m_mutex)> lock(m_mutex);
-        ++m_count;
-        m_cvar.notify_one();
-    }
-    void Wait() 
-    {
-        std::unique_lock<decltype(m_mutex)> lock(m_mutex);
-        while(!m_count)
-        {
-            m_cvar.wait(lock);
-        }
-        --m_count;
-    }
-    bool TryWait() 
-    {
-        std::lock_guard<decltype(m_mutex)> lock(m_mutex);
-        if(m_count) 
-        {
-            --m_count;
-            return true;
-        }
-        return false;
-    }
-};
-
 struct LockGuard
 {
     std::mutex& m_mutex;
@@ -47,5 +14,39 @@ struct LockGuard
     ~LockGuard()
     {
         m_mutex.unlock();
+    }
+};
+
+
+struct Semaphore
+{
+    std::mutex              m_mutex;
+    std::condition_variable m_cvar;
+    uint64_t                m_count = 0;
+
+    void Signal() 
+    {
+        LockGuard lock(m_mutex);
+        ++m_count;
+        m_cvar.notify_one();
+    }
+    void Wait() 
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        while(!m_count)
+        {
+            m_cvar.wait(lock);
+        }
+        --m_count;
+    }
+    bool TryWait() 
+    {
+        LockGuard lock(m_mutex);
+        if(m_count) 
+        {
+            --m_count;
+            return true;
+        }
+        return false;
     }
 };
