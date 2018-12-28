2bd64faf07f680e5fa38acf717450639cc2e51dc
Lauren Hughes <lh0xfb@gmail.com>
2018-12-27 20:04:53 -0800

    cleanup bsp2.h further

--- src/bsp2.h
+++ src/bsp2.h
@@ -1,23 +1,5 @@
 #pragma once
 
-// Original CSG.JS library by Evan Wallace (http://madebyevan.com), under the MIT license.
-// GitHub: https://github.com/evanw/csg.js/
-// 
-// C++ port by Tomasz Dabrowski (http://28byteslater.com), under the MIT license.
-// GitHub: https://github.com/dabroz/csgjs-cpp/
-// 
-// Constructive Solid Geometry (CSG) is a modeling technique that uses Boolean
-// operations like union and intersection to combine 3D solids. This library
-// implements CSG operations on meshes elegantly and concisely using BSP trees,
-// and is meant to serve as an easily understandable implementation of the
-// algorithm. All edge cases involving overlapping coplanar polygons in both
-// solids are correctly handled.
-//
-// To use this as a header file, define CSGJS_HEADER_ONLY before including this file.
-//
-
-// Edited again by Kara Hughes, under MIT license.
-
 #include "linmath.h"
 #include "array.h"
 
